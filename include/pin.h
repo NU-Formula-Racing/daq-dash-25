00bfd0c2848999e0a4df23fb5c90a48917f4df8c
nicoleseg <nicolesegarra2027@u.northwestern.edu>
2025-01-22 20:30:12 -0600

    pins for miso mosi in pin.h

--- include/pin.h
+++ include/pin.h
@@ -7,3 +7,12 @@
 
 #define rawRGB24toRGB565(r, g, b) uint16_t((r / 8) << 11) | ((g / 4) << 5) | (b / 8)
 #define NORTHWESTERN_PURPLE rawRGB24toRGB565(78, 42, 132)
+
+#define PIN_SPI_SS    (10)
+#define PIN_SPI_MOSI  (11)
+#define PIN_SPI_MISO  (12)
+#define PIN_SPI_SCK   (13)
+static const uint8_t SS = 10;
+static const uint8_t MOSI = 11;
+static const uint8_t MISO = 12;
+static const uint8_t SCK = 13;
