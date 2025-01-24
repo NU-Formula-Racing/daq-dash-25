c9e0aa08af01b3b4653bb472ac9d11c63dbcd9d7
nicoleseg <nicolesegarra2027@u.northwestern.edu>
2025-01-23 20:15:54 -0600

    pin change

--- include/pin.h
+++ include/pin.h
@@ -8,6 +8,7 @@
 #define rawRGB24toRGB565(r, g, b) uint16_t((r / 8) << 11) | ((g / 4) << 5) | (b / 8)
 #define NORTHWESTERN_PURPLE rawRGB24toRGB565(78, 42, 132)
 
+//pins_arduino.h
 #define PIN_SPI_SS    (10)
 #define PIN_SPI_MOSI  (11)
 #define PIN_SPI_MISO  (12)
@@ -17,6 +18,11 @@ static const uint8_t MOSI = 11;
 static const uint8_t MISO = 12;
 static const uint8_t SCK = 13;
 
-#define RA8875_INT 1
+//dash.h
+#define RA8875_INT 3
 #define RA8875_WAIT 2
 #define RA8875_RESET 4
+
+//other pins
+// drive can tx   1
+//drive can rx    0
