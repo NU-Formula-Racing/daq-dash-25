6d6846f798230a3944ba02b04f01234c49370462
Juliane Guo <jessicaguo934@gmail.com>
2025-02-27 21:14:51 -0600

    value and bytes converter completed

--- include/serializer.hpp
+++ include/serializer.hpp
@@ -10,10 +10,12 @@ using namespace std;
 struct CarData
 {
 public:
-    int wheelBackLeftSpeed; // 4 bytes
-    int wheelBackRightSpeed;
-    int wheelFrontLeftSpeed;
-    int wheelFrontRightSpeed;
+    float wheelSpeeds[4];
+    float wheelTemps[32];
+    float sus_displacement[4];
+    float sg_measurements[4];
+    float accel_vec[3];
+    float pos_vec[3];
 
     // for testing purposes, ignore
     bool operator==(const CarData &other) const {
@@ -32,8 +34,11 @@ public:
     CarData loadData(vector<uint8_t> data) const;
 
 private:
-    void addIntToBytes(vector<uint8_t> &byteData, int num) const;
-    int readIntFromBytes(vector<uint8_t> &byteData, int index) const;
+    // void addIntToBytes(vector<uint8_t> &byteData, int num) const;
+    // int readIntFromBytes(vector<uint8_t> &byteData, int index) consts;
+
+    void pushValueToBytes(vector<uint8_t> &buffer, void* value, size_t size) const;
+    void *readValueFromBytes(vector<uint8_t> &buffer, size_t *current_index, size_t size) const;
 };
 
 
--- src/serializer.cpp
+++ src/serializer.cpp
@@ -20,10 +20,15 @@ vector<uint8_t> Serializer::saveData(CarData data) const
     // push(third)
     // push(fourth)
 
-    addIntToBytes(byteData, data.wheelBackLeftSpeed);
-    addIntToBytes(byteData, data.wheelBackRightSpeed);
-    addIntToBytes(byteData, data.wheelFrontLeftSpeed);
-    addIntToBytes(byteData, data.wheelFrontRightSpeed);
+    for (int i = 0; i < 4; i++)
+    {
+        pushValueToBytes(byteData, &data.wheelSpeeds[i], sizeof(float));
+    }
+
+    // addIntToBytes(byteData, data.wheelBackLeftSpeed);
+    // addIntToBytes(byteData, data.wheelBackRightSpeed);
+    // addIntToBytes(byteData, data.wheelFrontLeftSpeed);
+    // addIntToBytes(byteData, data.wheelFrontRightSpeed);
 
     return byteData;
 }
@@ -32,45 +37,55 @@ vector<uint8_t> Serializer::saveData(CarData data) const
 CarData Serializer::loadData(vector<uint8_t> data) const
 {
     std::cout << "Serializer::loadData\n";
-    CarData intData;
+    CarData carData;
+    size_t currentIndex = 0;
 
-    intData.wheelBackLeftSpeed = readIntFromBytes(data, 0);
-    intData.wheelBackRightSpeed = readIntFromBytes(data, 4);
-    intData.wheelFrontLeftSpeed = readIntFromBytes(data, 8);
-    intData.wheelFrontRightSpeed = readIntFromBytes(data, 12);
-
-
-    // // a b c d ? ? ? ? ?
-    // intData.wheelBackLeftSpeed = static_cast<int>(data[0]);
-    // intData.wheelBackRightSpeed = static_cast<int>(data[1]);
-    // intData.wheelFrontLeftSpeed = static_cast<int>(data[2]);
+    for (int i = 0; i < 4; i++)
+    {
+        float *wheelSpeed = (float *)readValueFromBytes(data, &currentIndex, sizeof(float));
+        carData.wheelSpeeds[i] = *wheelSpeed;
+    }
 
     // // d ? ? ?
 
     // intData.wheelFrontRightSpeed = static_cast<int>(data[3]); // 4 thing in the array, and grab the next four bytes
 
-    return intData;
+    return carData;
 }
 
-
-void Serializer::addIntToBytes(vector<uint8_t> &byteData, int num) const
+void Serializer::pushValueToBytes(vector<uint8_t> &buffer, void* value, size_t size) const
 {
-    for (int i = 0; i < 4; i++)
-    {
-        byteData.push_back(num >> (i * 8));
+    uint8_t *data = static_cast<uint8_t*>(value);
+    for (size_t i = 0; i < size; i ++) {
+        uint8_t byte = data[i];
+        buffer.push_back(byte);
     }
 }
-// readIntFromBytes
 
-int Serializer::readIntFromBytes(vector<uint8_t> &byteData, int index) const
-{
-    int num = 0;
-
-    for (size_t i = 0; i < byteData.size(); i+=4)
-    {
-        num |= byteData[index]<<(i*8);
-    }
-
-    return num;
-
+void *Serializer::readValueFromBytes(vector<uint8_t> &buffer, size_t *current_index, size_t size) const {
+    void *data = (void *)(&buffer[*current_index]);
+    *current_index += size;
+    return data;
 }
+
+
+// void Serializer::addIntToBytes(vector<uint8_t> &byteData, int num) const
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         byteData.push_back(num >> (i * 8));
+//     }
+// }
+// // readIntFromBytes
+
+// int Serializer::readIntFromBytes(vector<uint8_t> &byteData, int index) const
+// {
+//     int num = 0;
+
+//     for (size_t i = 0; i < byteData.size(); i+=4)
+//     {
+//         num |= byteData[index]<<(i*8);
+//     }
+
+//     return num;
+// }
