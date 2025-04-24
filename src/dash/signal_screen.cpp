32901d0dd94a034566e01cb13151189e610b239c
SamanthaSu512 <yuyansu512@gmail.com>
2025-04-23 21:00:26 -0500

    Added SignalScreen

---
+++ src/dash/signal_screen.cpp
@@ -0,0 +1,11 @@
+#include "dash/signal_screen.h"
+#include "dash/drawer.h"
+#include "define.h"
+#include "resources.h"
+
+void SignalScreen::draw(Adafruit_RA8875 tft){
+   
+}
+void SignalScreen::update(Adafruit_RA8875 tft, bool force = false){
+
+}
