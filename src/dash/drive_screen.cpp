e172f3f4b9758c1f633159a4ab764bcc84f78dd3
evan-bertis-sample <esample21@gmail.com>
2025-06-13 02:11:19 -0500

    Info boxes!

--- src/dash/drive_screen.cpp
+++ src/dash/drive_screen.cpp
@@ -13,18 +13,78 @@ static const int infoHeight = 100;
 static const int infoLabelSize = 3;
 static const int infoValueSize = 3;
 
-
-static std::vector<std::string> infoLabelsLeft = {
-    "HV BAT V",
-    "LV BAT V",
-    "HV SOC"
+struct InfoBox {
+    const char *label;
+    std::function<float()> currentValue;
+    std::function<float()> previousValue;
+    float lowerBoundHard; // lower threshold for the value, show in red if below this
+    float lowerBoundSoft; // lower threshold for the value, show in yellow if below this
+    float upperBoundHard; // upper threshold for the value, show in red if above this
+    float upperBoundSoft; // upper threshold for the value, show in yellow if above this
 };
 
-static std::vector<std::string> infoLabelRight = {
-    "MIN CELL T",
-    "MAX CELL T",
-    "INV T"
+std::vector<InfoBox> leftBoxes = {
+    {
+        .label = "HV Voltage",
+        .currentValue = []() { return Resources::driveBusData().hvVoltage; },
+        .previousValue = []() { return Resources::prevDriveBusData().hvVoltage; },
+        .lowerBoundHard = 500,
+        .lowerBoundSoft = 530,
+        .upperBoundHard = 590,
+        .upperBoundSoft = 580,
+    },
+    {
+        .label = "LV Voltage",
+        .currentValue = []() { return Resources::driveBusData().lvVoltage; },
+        .previousValue = []() { return Resources::prevDriveBusData().lvVoltage; },
+        .lowerBoundHard = 20,
+        .lowerBoundSoft = 21,
+        .upperBoundHard = 24.5,
+        .upperBoundSoft = 24,
+    },
+    {
+        .label = "BMS SOC",
+        .currentValue = []() { return Resources::driveBusData().bmsSOC; },
+        .previousValue = []() { return Resources::prevDriveBusData().bmsSOC; },
+        .lowerBoundHard = 0,
+        .lowerBoundSoft = 10,
+        .upperBoundHard = 100,
+        .upperBoundSoft = 90,
+    }
+
 };
+
+std::vector<InfoBox> rightBoxes = {
+    {
+        .label = "Max Cell Temp",
+        .currentValue = []() { return Resources::driveBusData().maxCellTemp; },
+        .previousValue = []() { return Resources::prevDriveBusData().maxCellTemp; },
+        .lowerBoundHard = 60,
+        .lowerBoundSoft = 55,
+        .upperBoundHard = 100,
+        .upperBoundSoft = 90,
+    },
+    {
+        .label = "Min Cell Voltage",
+        .currentValue = []() { return Resources::driveBusData().minCellVoltage; },
+        .previousValue = []() { return Resources::prevDriveBusData().minCellVoltage; },
+        .lowerBoundHard = 2.5,
+        .lowerBoundSoft = 2.7,
+        .upperBoundHard = 4.2,
+        .upperBoundSoft = 4.1,
+    },
+    {
+        .label = "IGBT Temp",
+        .currentValue = []() { return Resources::driveBusData().inverterIGBTTemp; },
+        .previousValue = []() { return Resources::prevDriveBusData().inverterIGBTTemp; },
+        .lowerBoundHard = 15,
+        .lowerBoundSoft = 20,
+        .upperBoundHard = 100,
+        .upperBoundSoft = 90,
+    }
+};
+
+
 
 static uint16_t getDriveStateColor() {
     switch (Resources::driveBusData().driveState) {
@@ -210,6 +270,127 @@ static void drawLoggerStatus(Adafruit_RA8875 tft) {
     }
 }
 
+static uint16_t getInfoBoxColor(InfoBox &box) {
+    float value = box.currentValue();
+    if (value < box.lowerBoundHard || value > box.upperBoundHard) {
+        return GOTH_RED; // hard bounds
+    } else if (value < box.lowerBoundSoft || value > box.upperBoundSoft) {
+        return GOLD; // soft bounds
+    } else {
+        return GOTH_GREEN; // normal
+    }
+}
+
+
+static void drawInfoBoxes(Adafruit_RA8875 tft, bool force) {
+
+    int infoAvailableVerticalSpace = SCREEN_HEIGHT - infoPaddingVertical * 2;
+    int infoFullVerticalSpace = leftBoxes.size() * infoHeight;
+    int infoGap = (infoAvailableVerticalSpace - infoFullVerticalSpace) / (leftBoxes.size() - 1);
+
+    // draw boxes for the labels and values
+    for (size_t i = 0; i < leftBoxes.size(); i++) {
+        InfoBox &box = leftBoxes[i];
+
+        // check if there is enough of a change in the value to warrant a redraw
+        if (!force && abs(box.currentValue() - box.previousValue()) < 0.01) {
+            continue; // no need to redraw
+        }
+
+        int y = infoPaddingVertical + i * (infoHeight + infoGap) + infoHeight / 2;
+
+        Drawer::drawRect(tft, (RectDrawOptions){
+                                  .x = infoPaddingSides + infoWidth / 2,
+                                  .y = y,
+                                  .width = infoWidth,
+                                  .height = infoHeight,
+                                  .fill = true,
+                                  .strokeThickness = 10,
+                                  .strokeColor = OUTLINE_COLOR,
+                                  .fillColor = getInfoBoxColor(box),
+                                  .cornerRadius = 15,
+                                  .hAlign = ALIGN_CENTER,
+                                  .vAlign = ALIGN_MIDDLE,
+                              });
+
+        Drawer::drawString(tft, box.label,
+                           (TextDrawOptions){
+                               .x = infoPaddingSides + infoWidth / 2,
+                               .y = y - infoHeight / 4,
+                               .size = infoLabelSize,
+                               .color = GOTH_WHITE,
+                               .backgroundColor = getInfoBoxColor(box),
+                               .hAlign = ALIGN_CENTER,
+                               .vAlign = ALIGN_MIDDLE,
+                           });
+
+        Drawer::drawNum(tft, box.currentValue(),
+                        (NumberDrawOptions){
+                            .x = infoPaddingSides + infoWidth / 2,
+                            .y = y + infoHeight / 4,
+                            .size = infoValueSize,
+                            .color = GOTH_WHITE,
+                            .backgroundColor = getInfoBoxColor(box),
+                            .precision = 2,
+                            .hAlign = ALIGN_CENTER,
+                            .vAlign = ALIGN_MIDDLE});
+    }
+
+    // do the same for the right side
+    infoAvailableVerticalSpace = SCREEN_HEIGHT - infoPaddingVertical * 2;
+    infoFullVerticalSpace = rightBoxes.size() * infoHeight;
+    infoGap = (infoAvailableVerticalSpace - infoFullVerticalSpace) / (rightBoxes.size() - 1);
+
+    for (size_t i = 0; i < rightBoxes.size(); i++) {
+        InfoBox &box = rightBoxes[i];
+
+        // check if there is enough of a change in the value to warrant a redraw
+        if (!force && abs(box.currentValue() - box.previousValue()) < 0.01) {
+            continue; // no need to redraw
+        }
+
+        int y = infoPaddingVertical + i * (infoHeight + infoGap) + infoHeight / 2;
+
+        Drawer::drawRect(tft, (RectDrawOptions){
+                                  .x = SCREEN_WIDTH - infoPaddingSides - infoWidth / 2,
+                                  .y = y,
+                                  .width = infoWidth,
+                                  .height = infoHeight,
+                                  .fill = true,
+                                  .strokeThickness = 10,
+                                  .strokeColor = OUTLINE_COLOR,
+                                  .fillColor = getInfoBoxColor(box),
+                                  .cornerRadius = 15,
+                                  .hAlign = ALIGN_CENTER,
+                                  .vAlign = ALIGN_MIDDLE,
+                              });
+
+        Drawer::drawString(tft, box.label,
+                           (TextDrawOptions){
+                               .x = SCREEN_WIDTH - infoPaddingSides - infoWidth / 2,
+                               .y = y - infoHeight / 4,
+                               .size = infoLabelSize,
+                               .color = GOTH_WHITE,
+                               .backgroundColor = getInfoBoxColor(box),
+                               .hAlign = ALIGN_CENTER,
+                               .vAlign = ALIGN_MIDDLE,
+                           });
+
+        Drawer::drawNum(tft, box.currentValue(),
+                        (NumberDrawOptions){
+                            .x = SCREEN_WIDTH - infoPaddingSides - infoWidth / 2,
+                            .y = y + infoHeight / 4,
+                            .size = infoValueSize,
+                            .color = GOTH_WHITE,
+                            .backgroundColor = getInfoBoxColor(box),
+                            .precision = 2,
+                            .hAlign = ALIGN_CENTER,
+                            .vAlign = ALIGN_MIDDLE});
+    }
+
+}
+
+
 void DriveScreen::draw(Adafruit_RA8875 tft) {
     Serial.print("Drawing DriveScreen!");
     tft.fillScreen(BACKGROUND_GRAY);
@@ -227,58 +408,7 @@ void DriveScreen::draw(Adafruit_RA8875 tft) {
     options.vAlign = ALIGN_MIDDLE;
     Drawer::drawRect(tft, options);
 
-    // draw info labels on the left and right side
-    // distribute them evenly vertically
-    // left labels are aligned left horizontally
-    // right labels are aligned right horizontally
-
-    // total amount of space that we can distribute the labels in
-    // draw the boxes for the labels + values
-
-    int infoAvailableVerticalSpace = SCREEN_HEIGHT - infoPaddingVertical * 2;
-    int infoFullVerticalSpace = infoLabelsLeft.size() * infoHeight;
-    int infoGap = (infoAvailableVerticalSpace - infoFullVerticalSpace) / (infoLabelsLeft.size() - 1);
-
-    // draw boxes for the labels and values
-    for (size_t i = 0; i < infoLabelsLeft.size(); i++) {
-        int y = infoPaddingVertical + i * (infoHeight + infoGap) + infoHeight / 2;
-
-        Drawer::drawRect(tft, (RectDrawOptions){
-                                  .x = infoPaddingSides + infoWidth / 2,
-                                  .y = y,
-                                  .width = infoWidth,
-                                  .height = infoHeight,
-                                  .fill = true,
-                                  .strokeThickness = 10,
-                                  .strokeColor = OUTLINE_COLOR,
-                                  .fillColor = GOTH_GREEN,
-                                  .cornerRadius = 15,
-                                  .hAlign = ALIGN_CENTER,
-                                  .vAlign = ALIGN_MIDDLE,
-                              });
-
-        Drawer::drawString(tft, infoLabelsLeft[i],
-                           (TextDrawOptions){
-                               .x = infoPaddingSides + infoWidth / 2,
-                               .y = y - infoHeight / 4,
-                               .size = infoLabelSize,
-                               .color = GOTH_WHITE,
-                               .backgroundColor = GOTH_GREEN,
-                               .hAlign = ALIGN_CENTER,
-                               .vAlign = ALIGN_MIDDLE,
-                           });
-
-        Drawer::drawNum(tft, Resources::driveBusData().hvVoltage,  // TODO: change to correct data
-                        (NumberDrawOptions){
-                            .x = infoPaddingSides + infoWidth / 2,
-                            .y = y + infoHeight / 4,
-                            .size = infoValueSize,
-                            .color = GOTH_WHITE,
-                            .backgroundColor = GOTH_GREEN,
-                            .precision = 2,
-                            .hAlign = ALIGN_CENTER,
-                            .vAlign = ALIGN_MIDDLE});
-    }
+    drawInfoBoxes(tft, true);
 }
 
 void DriveScreen::update(Adafruit_RA8875 tft, bool force) {
@@ -292,45 +422,8 @@ void DriveScreen::update(Adafruit_RA8875 tft, bool force) {
         drawSpeed(tft);
     }
 
-    // // Update high-voltage battery status.
-    // if (Resources::driveBusData().hvVoltage != Resources::prevDriveBusData().hvVoltage  || force) {
-    //     drawCircleStatus(tft,
-    //                      hv_bat_volt_startX,
-    //                      hv_bat_volt_startY,
-    //                      Resources::driveBusData().hvVoltage,
-    //                      hv_battery_voltage_mid_state,
-    //                      hv_battery_voltage_last_state);
-    // }
+    drawInfoBoxes(tft, force);
 
-    // // Update low-voltage battery status.
-    // if (Resources::driveBusData().lvVoltage != Resources::prevDriveBusData().lvVoltage || force) {
-    //     drawCircleStatus(tft,
-    //                      lv_bat_volt_startX,
-    //                      lv_bat_volt_startY,
-    //                      Resources::driveBusData().lvVoltage,
-    //                      lv_battery_voltage_mid_state,
-    //                      lv_battery_voltage_last_state);
-    // }
-
-    // // Update maximum cell temperature display.
-    // if (Resources::driveBusData().maxCellTemp != Resources::prevDriveBusData().maxCellTemp || force) {
-    //     drawCircleStatus(tft,
-    //                      max_cell_temp_startX,
-    //                      max_cell_temp_startY,
-    //                      Resources::driveBusData().maxCellTemp,  // Change to maxCellTemp if defined separately.
-    //                      max_cell_temp_mid_state,
-    //                      max_cell_temp_last_state);
-    // }
-
-    // // Update minimum cell temperature display.
-    // if (Resources::driveBusData().minCellVoltage - Resources::prevDriveBusData().minCellVoltage || force) {
-    //     drawCircleStatus(tft,
-    //                      min_cell_temp_startX,
-    //                      min_cell_temp_startY,
-    //                      Resources::driveBusData().minCellVoltage,
-    //                      min_cell_temp_mid_state,
-    //                      min_cell_temp_last_state);
-    // }
 }
 
 void DriveScreen::periodicDraw(Adafruit_RA8875 tft) {
